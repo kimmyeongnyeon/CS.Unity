ba86b109c701363fac1be3707483d38d5d6f4f02
kimmyeongnyeon <66676451+kimmyeongnyeon@users.noreply.github.com>
2020-10-06 00:44:00 +0900

    텍스쳐변경(Texture Change)

---
+++ FilledChange.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FilledChange : MonoBehaviour
+{
+    public Texture[] textures;// 이미지 배열 선언
+    private MeshRenderer meshRenderer;
+
+ 
+ void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "PLAYER")
+        {
+            Debug.Log(collision.gameObject.tag.ToString());
+            meshRenderer.material.mainTexture = textures[0];
+        }
+    }
+
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        //meshRenderer.material.mainTexture = textures[0];
+        Debug.Log("start");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
